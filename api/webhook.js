14507eaf8bbdbae59c582c73cdd711d6d095fea8
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-08-07 12:49:58 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 // La URL y la clave de Supabase se cargan desde las variables de entorno de Vercel
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default async function handler(req, res) {
@@ -122,3 +122,4 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Error guardando en Supabase" });
   }
 }
+
